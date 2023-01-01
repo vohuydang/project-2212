f2b3f09f535e70fa03d3c51e12c37053b53ae2f9
heeyoung-choi <oprocinante@gmail.com>
2023-01-01 17:10:04 +0700

    initial commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -127,7 +127,7 @@ const IndexPage = () => {
   return (
     <main style={pageStyles}>
       <h1 style={headingStyles}>
-        Congratulations
+        Xin chao nhu Huynh
         <br />
         <span style={headingAccentStyles}>— you just made a Gatsby site! 🎉🎉🎉</span>
       </h1>
